c410a97fa512629d2e5a9cab910998daa80a0494
jdhayhurst <hayhurst.jd@gmail.com>
2019-07-29 17:10:48 +0100

    updating tests

--- tests/test_file_handler.py
+++ tests/test_file_handler.py
@@ -10,15 +10,16 @@ class TestSumStatsFile(unittest.TestCase):
         self.test_storepath = "./tests/data"
         config.STORAGE_PATH = self.test_storepath
         self.cid = "TiQS2yxV"
+        self.sid = "mKoYvoLH8L"
 
     def tearDown(self):
         shutil.rmtree(self.test_storepath)
 
     def test_make_parent_dir(self):
-        ssf = fh.SumStatFile(callback_id=self.cid)
-        self.assertFalse(os.path.exists(ssf.store_path))
+        ssf = fh.SumStatFile(callback_id=self.cid, study_id=self.sid)
+        self.assertFalse(os.path.exists(ssf.parent_path))
         ssf.make_parent_dir()
-        self.assertTrue(os.path.exists(ssf.store_path))
+        self.assertTrue(os.path.exists(ssf.parent_path))
 
 
 
