55dc5026db323477d7681bfee69a0a47c13c6789
jdhayhurst <38317975+jdhayhurst@users.noreply.github.com>
2019-09-13 18:09:14 +0100

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,17 @@
 import pytest
+import os
 
 
 @pytest.fixture(scope='session')
 def celery_config():
     return {
-            'broker_url': 'amqp://guest@localhost:5682',
+            'broker_url': '{msg_protocol}://{user}:{pwd}@{host}:{port}//'.format(
+                msg_protocol=os.environ['CELERY_PROTOCOL'],
+                user=os.environ['CELERY_USER'],
+                pwd=os.environ['CELERY_PASSWORD'],
+                host=os.environ['QUEUE_HOST'],
+                port=os.environ['QUEUE_PORT']
+                ),
             'result_backend': 'rpc://',
             'queues':('preval', 'postval')
            }
