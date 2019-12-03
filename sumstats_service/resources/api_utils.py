3acd28d5f692248997bf5f2967269c8751fb7570
jdhayhurst <hayhurst.jd@gmail.com>
2019-12-03 10:56:00 +0000

    setting singularity cache

--- sumstats_service/resources/api_utils.py
+++ sumstats_service/resources/api_utils.py
@@ -38,8 +38,8 @@ def validate_files_from_payload(callback_id, content):
         outfile = os.path.join(par_dir, 'validation.json')
         memory = 4000
         content = json.dumps(content).translate(str.maketrans({'"':  '\\"'}))
-        bsub_com = 'singularity exec --bind {sp} docker://{image}:{tag} validate-payload -cid {cid} -out {outfile} -storepath {sp} -payload \'{content}\''.format(
-                image=config.SINGULARITY_IMAGE, tag=config.SINGULARITY_TAG, cid=callback_id, outfile=outfile, sp=config.STORAGE_PATH, content=content)
+        bsub_com = 'SINGULARITY_CACHEDIR={cache} singularity exec --home {home} --bind {sp} docker://{image}:{tag} validate-payload -cid {cid} -out {outfile} -storepath {sp} -payload \'{content}\''.format(
+                image=config.SINGULARITY_IMAGE, tag=config.SINGULARITY_TAG, cid=callback_id, outfile=outfile, sp=config.STORAGE_PATH, content=content, cache=config.CACHE_PATH, home=config.USERHOME)
         command = 'export http_proxy={hp}; export https_proxy={hsp}; mkdir -p {pd}; bsub -oo {pd}/stdout -eo {pd}/stderr -M {mem} -R "rusage[mem={mem}]" "{bsub_com}"'.format(
                 pd=par_dir, q=config.COMPUTE_FARM_QUEUE, mem=memory, bsub_com=bsub_com, hp=config.HTTP_PROXY, hsp=config.HTTPS_PROXY)
         stdin, stdout, stderr = ssh.exec_command(command)
