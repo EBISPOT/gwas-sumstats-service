359472a4fd784cce4234caff0ffbf0de4831b562
jdhayhurst <hayhurst.jd@gmail.com>
2023-06-20 09:01:03 +0100

    bump version

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='gwas-sumstats-service',
-    version='v1.3.6',
-    packages=find_packages(include=['sumstats_service']),
+    name="gwas-sumstats-service",
+    version="v1.3.7",
+    packages=find_packages(include=["sumstats_service"]),
     entry_points={
-        "console_scripts": 
-            ['validate-payload = sumstats_service.resources.validate_payload:main',
-             'validate-study = sumstats_service.resources.validate_study:main',
-             'convert_meta = sumstats_service.resources.convert_meta:main'
-            ]
+        "console_scripts": [
+            "validate-payload = sumstats_service.resources.validate_payload:main",
+            "validate-study = sumstats_service.resources.validate_study:main",
+            "convert_meta = sumstats_service.resources.convert_meta:main",
+        ]
     },
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'flask',
-    ]
+        "flask",
+    ],
 )
