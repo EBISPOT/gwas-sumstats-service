bbb1db36d0029ac48ae9a9f3f920f9937d2d4226
Karatuğ Ozan Bircan <karatugo@ebi.ac.uk>
2023-12-21 15:21:33 +0000

    test: Use only the custom config

--- sumstats_service/resources/api_utils.py
+++ sumstats_service/resources/api_utils.py
@@ -220,7 +220,7 @@ def nextflow_command_string(
         f"--zerop {zero_p_values} "
         f"--validatedPath {config.VALIDATED_PATH} "
         f"-w {wd} "
-        f"-c {nextflow_config_path} "
+        f"-C {nextflow_config_path} "
         f"-with-singularity {config.SINGULARITY_IMAGE}_{config.SINGULARITY_TAG}.sif"
     )
     if containerise is False:
